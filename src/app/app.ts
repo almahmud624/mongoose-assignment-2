ae69f690d2e3699a9dca7ee9551920ee99f9b5fc
almahmud624 <mahmudbhuiyan624@gmail.com>
2023-12-08 09:48:03 +0600

    remove v1 from route

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,7 +8,7 @@ const router = express.Router();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/v1/users", userRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Assignment");
